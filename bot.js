0491dc5132adf3dfdd8c583633162f7a550f0281
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-03 15:23:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,43 +3,40 @@ const client = new Discord.Client();
 const prefix = '!';
 client.on('ready', () => console.log('Im Ready For Sell! AG STORE'));
 
-client.on('message', async msg => {
-			let vip;
-			let price;
-			let desc;
-			let c = msg.guild.channels.find(c => c.name === "vip-shop")
-			if (msg.content === '!sellv') {
-				msg.channel.send("ماذا تبيع ؟").then(ms => {
-						let c = await msg.channel.awaitMessages(m => m.id = msg.author.id, {
-							time: 5000,
-							max: 1
-						}).then(co => {
-							if (co.first().length < 1) return;
-							vip = co.first().content
-						})
-						ms.edit("السعر ?")
-						let cp = await msg.channel.awaitMessages(m => m.id = msg.author.id, {
-							time: 5000,
-							max: 1
-						}).then(co => {
-							if (co.first().length < 1) return;
-							price = co.first().content
-						})
-						ms.edit("الوصف ?")
-						let cd = await msg.channel.awaitMessages(m => m.id = msg.author.id, {
-							time: 5000,
-							max: 1
-						}).then(co => {
-							if (co.first().length < 1) return;
-							decs = co.first().content
-						})
-						var embed = new Discord.RichEmbed().setTitle('**AG VIP STORE**').setColor("RANDOM").setThumbnail(`${msg.author.avatarURL}`).addField('البائع', `<@${msg.author.id}>`).addField('السلعة', vip).addField('السعر', price).addField('الوصف', desc);
-						msg.channel.send(`**تم نشر السلعة** :white_check_mark: `)
-						msg.delete();
-						c.send("@everyone", {
-							embed
-						})
-					}
-				});
 
+client.on('message', async msg => {
+let vip;
+let price;
+let desc;
+let c = msg.guild.channels.find(c => c.name === "vip-shop")
+  if (msg.content === '!sellv') {
+msg.channel.send("ماذا تبيع ؟").then(ms => {
+    let c = await msg.channel.awaitMessages(m => m.id = msg.author.id, { time: 5000, max: 1 }).then(co => {
+if(co.first().length < 1) return;
+vip = co.first().content
+})
+ ms.edit("السعر ?")
+ let cp = await msg.channel.awaitMessages(m => m.id = msg.author.id, { time: 5000, max: 1 }).then(co => {
+if(co.first().length < 1) return;
+price = co.first().content
+})
+ ms.edit("الوصف ?")
+ let cd = await msg.channel.awaitMessages(m => m.id = msg.author.id, { time: 5000, max: 1 }).then(co => {
+if(co.first().length < 1) return;
+desc = co.first().content
+ })
+        var embed = new Discord.RichEmbed()
+        .setTitle('**AG VIP STORE**')
+        .setColor("RANDOM")
+        .setThumbnail(`${msg.author.avatarURL}`)
+        .addField('البائع', `<@${msg.author.id}>`)
+        .addField('السلعة', vip)
+        .addField('السعر', price)    
+        .addField('الوصف', desc);
+            msg.channel.send(`**تم نشر السلعة** :white_check_mark: `)
+            msg.delete();
+        c.send("@everyone", {embed})
+    }
+});
+	  
 client.login(process.env.BOT_TOKEN); 
