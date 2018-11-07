d052875ef5f86eaf4663c39029ca400995b68fe5
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-07 20:56:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,19 +16,19 @@ let desc;
       if (message.author.bot) return
                                      }
  let ms = message.channel.send({embed: new Discord.RichEmbed().setTitle('ماذا تريد ان تبيع؟').setColor(0xFF0000)}).then(function(ms) {
- let co = await message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(function(co) => {
+ let co = message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(co => {
 if(co.first().length < 1) return;
 vip = co.first().content
  },
     setTimeout(function() {
     ms.edit({embed: new Discord.RichEmbed().setTitle('السعر؟').setColor(0xFF0000)})
-   let cp = await message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(co => {
+   let cp = message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(co => {
 if(co.first().length < 1) return;
 price = co.first().content
              },
             setTimeout(function() {
                ms.edit({embed: new Discord.RichEmbed().setTitle('وصف السلعة؟').setColor(0xFF0000)})
-      let cd = await message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(co => {
+      let cd = message.channel.awaitMessages(m => m.id = message.author.id, { time: 5000, max: 1 }).then(co => {
 if(co.first().length < 1) return;
 desc = co.first().content
              },
