221493fb7c141e2cd0454fe0fdf6375b081e3732
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-07 21:02:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ price = co.first().content
  ms.edit("الوصف ?")
  let cd = msg.channel.awaitMessages(m => m.id = msg.author.id, { time: 5000, max: 1 }).then(co => {
 if(co.first().length < 1) return;
-decs = co.first().content
+desc = co.first().content
  })
         var embed = new Discord.RichEmbed()
         .setTitle('**AG VIP STORE**')
@@ -38,7 +38,8 @@ decs = co.first().content
             msg.channel.send(`**تم نشر السلعة** :white_check_mark: `)
             msg.delete();
         c.send({embed})
-    
+    });
+  }
 });
 
 client.login(process.env.BOT_TOKEN); 
